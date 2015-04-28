5327f210b773a3c308ee3c76afdf83980bdb6362
andigaluh <andi@komunigrafik.com>
2015-04-28 15:42:32 +0700

    git ignore file

--- application/config/assets.php
+++ application/config/assets.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['assets_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/projects/erl-frontend/assets/';
+$config['assets_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/_zohobiz_/assets/';
 
 /* End of file assets.php */
 /* Location: ./application/config/assets.php */
